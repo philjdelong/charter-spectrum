f9f7f333a9b940ee19217e9939ffc8afde8ebe90
Phil DeLong <philjdelong@gmail.com>
2020-11-17 16:07:35 -0700

    Render all restaurants in table, sorted alphabetically

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import env from 'dotenv'
 env.config();
 
 class App extends Component {
+  state = {
+    restaurants: []
+  }
   componentDidMount() {
     axios.get("https://code-challenge.spectrumtoolbox.com/api/restaurants", {
       headers: { 
@@ -13,18 +16,41 @@ class App extends Component {
       }
     })
       .then(result => {
-        console.log(result.data)
-      })
-  }
+        const sortedData = result.data.sort((a,b) => {
+          return a.name.localeCompare(b.name);
+        })
+        this.setState({restaurants: sortedData})
+      });
+  };
 
 
   render() {
+    const restaurants = this.state.restaurants.map(restaurant => {
+      return(
+        <div>
+          <tbody className="restaurant">
+            <td className="cell-name">{restaurant.name}</td>
+            <td className="cell-city">{restaurant.city}</td>
+            <td className="cell-state">{restaurant.state}</td>
+            <td className="cell-phone">{restaurant.telephone}</td>
+            <td className="cell-genre">{restaurant.genre}</td>
+          </tbody>
+        </div>
+      )
+    })
     return (
       <div className="App">
         <header className="App-header">
           Charter-Spectrum FE Code Challenge
+        <tbody>
+          {/* <th>Name</th>
+          <th>City</th>
+          <th>State</th>
+          <th>Telephone</th>
+          <th>Genre</th> */}
+          {restaurants}
+        </tbody>  
         </header>
-        {/* <Table /> */}
       </div>
     );
   }
