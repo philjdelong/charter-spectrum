2e7706d1f6eed44d0a1e8cc738d98935c5306fea
Phil DeLong <philjdelong@gmail.com>
2020-11-17 14:48:13 -0700

    Fetch endpoint data with axios and log result

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import axios from 'axios'
+import env from 'dotenv'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+env.config();
+
+class App extends Component {
+  componentDidMount() {
+    axios.get("https://code-challenge.spectrumtoolbox.com/api/restaurants", {
+      headers: { 
+        "Authorization": "Api-Key q3MNxtfep8Gt"
+      }
+    })
+      .then(result => {
+        console.log(result.data)
+      })
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Charter-Spectrum FE Code Challenge
+        </header>
+        {/* <Table /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
