af661b1397f2d2531d9c2cb5161740a366c38f2d
Phil DeLong <philjdelong@gmail.com>
2020-11-19 11:36:24 -0700

    Update title and move styling components into separate dirs

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,7 @@
 const header = props => {
     return(
         <div>
-            <h1>Charter-Spectrum FE Code Challenge</h1>
+            <h1>Restaurant RoundUp</h1>
             <p>Filter by State (abbreviation):<br></br><input onChange={props.stateFilter}></input><button onClick={props.clearFilter}>Clear</button></p>
         </div>
     );
--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -1,13 +1,13 @@
+import '../Table/Table.css'
+
 const table = props => {
     return(
-        <div>
-            <table className="restaurants">
-              <th>Name,</th>
-              <th>City,</th>
-              <th>State,</th>
-              <th>Telephone,</th>
-              <th>Genre</th>
-            </table>
+        <div className="table-heading">
+            <th>Name,</th>
+            <th>City,</th>
+            <th>State,</th>
+            <th>Telephone</th>
+            {/* <th>Genre</th> */}
         </div>
     );
 };
