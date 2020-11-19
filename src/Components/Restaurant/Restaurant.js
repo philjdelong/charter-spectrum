9a79e9e993ed3b3835301ef5272013b47f88614b
Phil DeLong <philjdelong@gmail.com>
2020-11-19 13:59:04 -0700

    Fix DOM rendering bug

--- src/Components/Restaurant/Restaurant.js
+++ src/Components/Restaurant/Restaurant.js
@@ -2,15 +2,17 @@ import '../Restaurant/Restaurant.css'
 
 const restaurant = props => {
     return(
-        <div>
+        <table>
             <tbody>
-                <td className="name">{props.name}</td>
-                <td className="cell">{props.city}</td>
-                <td className="state">{props.state}</td>
-                <td className="cell">{props.telephone}</td>
-                {/* <td className="cell-genre">{props.genre}</td> */}
+                <tr>
+                    <td className="name">{props.name}</td>
+                    <td className="cell">{props.city}</td>
+                    <td className="state">{props.state}</td>
+                    <td className="cell">{props.telephone}</td>
+                    {/* <td className="cell-genre">{props.genre}</td> */}
+                </tr>
             </tbody>
-        </div>
+        </table>
     );
 };
 
