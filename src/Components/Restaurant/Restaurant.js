b99b59478eebda493d04906cc467d1991aee8423
Phil DeLong <philjdelong@gmail.com>
2020-11-19 13:38:42 -0700

    Remove multiple class names

--- src/Components/Restaurant/Restaurant.js
+++ src/Components/Restaurant/Restaurant.js
@@ -4,9 +4,9 @@ const restaurant = props => {
     return(
         <div>
             <tbody>
-                <td className="cell" className="name">{props.name}</td>
+                <td className="name">{props.name}</td>
                 <td className="cell">{props.city}</td>
-                <td className="cell" className="state">{props.state}</td>
+                <td className="state">{props.state}</td>
                 <td className="cell">{props.telephone}</td>
                 {/* <td className="cell-genre">{props.genre}</td> */}
             </tbody>
