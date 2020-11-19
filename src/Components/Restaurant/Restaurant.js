fd3fcfbe157a25e441580ddae80dc5d5825c3301
Phil DeLong <philjdelong@gmail.com>
2020-11-19 11:23:28 -0700

    Refactor class tags and move unique styling into component dirs

--- src/Components/Restaurant/Restaurant.js
+++ src/Components/Restaurant/Restaurant.js
@@ -1,12 +1,14 @@
+import '../Restaurant/Restaurant.css'
+
 const restaurant = props => {
     return(
         <div>
             <tbody>
-                <td className="cell-name">{props.name}</td>
+                <td className="cell" className="name">{props.name}</td>
                 <td className="cell">{props.city}</td>
-                <td className="cell-state">{props.state}</td>
+                <td className="cell" className="state">{props.state}</td>
                 <td className="cell">{props.telephone}</td>
-                <td className="cell-genre">{props.genre}</td>
+                {/* <td className="cell-genre">{props.genre}</td> */}
             </tbody>
         </div>
     );
