c4a2e48e9fa4629012c1bbc97f5f5b6717882861
Phil DeLong <philjdelong@gmail.com>
2020-11-19 16:26:44 -0700

    Include table elements with tbody to fix err when rendering

--- src/Components/Restaurant/Restaurant.js
+++ src/Components/Restaurant/Restaurant.js
@@ -2,17 +2,20 @@ import '../Restaurant/Restaurant.css'
 
 const restaurant = props => {
     return(
+        <div>
+
         <table>
-            <tbody>
+            <tbody className="all-cells">
                 <tr>
-                    <td className="name">{props.name}</td>
-                    <td className="cell">{props.city}</td>
-                    <td className="state">{props.state}</td>
-                    <td className="cell">{props.telephone}</td>
+                    <td className="cell-name">{props.name}</td>
+                    <td className="cell-default">{props.city}</td>
+                    <td className="cell-state">{props.state}</td>
+                    <td className="cell-default">{props.telephone}</td>
                     {/* <td className="cell-genre">{props.genre}</td> */}
                 </tr>
             </tbody>
         </table>
+        </div>
     );
 };
 
